841c8427425f90e5ab600254b8d2f2c296d3e8e7
Atif01-AH <hayatatif7777@gmail.com>
2025-11-12 16:14:37 +0530

    Update problem1.c

--- assignment3.2/problem1.c
+++ assignment3.2/problem1.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <math.h>
-int main(){
-       float a,b,c,Discriminant,root1,root2,realroots,imaginaryroots;
-       printf("Enter coefficient a : ");
-       scanf("%f",&a);
-       printf("Enter coefficient b : ");
-       scanf("%f",&b);
-       printf("Enter coefficient c : ");
-       scanf("%f",&c);
-       Discriminant=b*b-4*a*c;
 
-       if(Discriminant>0){
+int main() {
+    float a, b, c, Discriminant, root1, root2, realroots, imaginaryroots;
 
-          root1=(-b+ sqrt(Discriminant))/(2*a);
-          root2=(-b- sqrt(Discriminant))/(2*a);
+    
+    printf("Enter coefficient a: ");
+    scanf("%f", &a);
+    printf("Enter coefficient b: ");
+    scanf("%f", &b);
+    printf("Enter coefficient c: ");
+    scanf("%f", &c);
 
-        printf("two real and distinct roots : %f\n %f",root1,root2);
+   
+    Discriminant = b * b - 4 * a * c;
 
-       }
+    
+    if (Discriminant > 0) {
+        root1 = (-b + sqrt(Discriminant)) / (2 * a);
+        root2 = (-b - sqrt(Discriminant)) / (2 * a);
+        printf("Two real and distinct roots: %.2f and %.2f\n", root1, root2);
+    }
+    
+    else if (Discriminant == 0) {
+        root1 = root2 = -b / (2 * a);
+        printf("Two real and equal roots: %.2f\n", root1);
+    }
+    
+    else {
+        realroots = -b / (2 * a);
+        imaginaryroots = sqrt(-Discriminant) / (2 * a);
+        printf("Roots are imaginary and complex: %.2f + %.2fi and %.2f - %.2fi\n", realroots, imaginaryroots, realroots, imaginaryroots);
+    }
 
-       else if(Discriminant==0){
-
-            root1=root2=-b/(2*a);
-
-        printf("two real and equal roots : %f",root1);
-       }
-
-       else{
-            realroots=-b/(2*a);
-
-            imaginaryroots= sqrt(-Discriminant)/(2*a);
-
-        printf("roots are imaginary and complex : %f",imaginaryroots);
-
-
-       }
-       return 0;
+    return 0;
 }
+
