87e87658365e73a5dd22b8ac94273bcfb37a8cf5
Atif01-AH <hayatatif7777@gmail.com>
2026-01-16 16:19:18 +0530

    Update assignment03 Q5.c

--- Assignment3.1/assignment03 Q5.c
+++ Assignment3.1/assignment03 Q5.c
@@ -1,9 +1,24 @@
-#include<stdio.h>
-int main(){
-  int mathematics,physics,chemistry;
-  printf("Enter the marks obtained in mathematics");
-  scanf("%d",mathematics);
+#include <stdio.h>
 
+int main() {
+    int math, physics, chemistry, total, mp_total;
+    printf("Enter marks in Mathematics: ");
+    scanf("%d", &math);
+    printf("Enter marks in Physics: ");
+    scanf("%d", &physics);
+    printf("Enter marks in Chemistry: ");
+    scanf("%d", &chemistry);
+  
+    // Calculate total marks
+    total = math + physics + chemistry;
+    mp_total = math + physics;
 
+    if (math >= 65 && physics >= 55 && chemistry >= 50 &&
+        (total >= 190 || mp_total >= 140)) {
+        printf("Eligible for admission\n");
+    } else {
+        printf("Not eligible for admission\n");
+    }
 
+    return 0;
 }
