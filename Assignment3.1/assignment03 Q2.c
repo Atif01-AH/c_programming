9e7007526cc8f8a1231cb80e036f069cc776f962
Atif01-AH <hayatatif7777@gmail.com>
2026-01-16 16:13:33 +0530

    Assign value to n based on m's value
    Set the value of n based on the value of m and print it.

--- Assignment3.1/assignment03 Q2.c
+++ Assignment3.1/assignment03 Q2.c
@@ -5,17 +5,18 @@ int main(){
  scanf("%d",&n);
 
  if(m>0){
-    printf("n is 1");
+   n = 1;
  }
  else if(m==0){
-        printf("n is 0");
+        n = 0;
  }
  else if(m<0){
-    printf("n is -1");
+    n = -1;
  }
  else {
     printf("invalid");
  }
-
+printf("Value of n = %d",n);
+ return 0;
 
 }
