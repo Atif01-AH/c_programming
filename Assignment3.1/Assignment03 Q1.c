260dd1cba611c4df8df1c68bd9cc4179516c7f5f
Atif01-AH <hayatatif7777@gmail.com>
2026-01-16 16:11:26 +0530

    Refactor leap year calculation logic

--- Assignment3.1/Assignment03 Q1.c
+++ Assignment3.1/Assignment03 Q1.c
@@ -1,16 +1,16 @@
-#include<stdio.h>
-int main(){
-  int year;
-  printf("Enter the year: ");
-  scanf("%d",&year);
-  if(year%4==0){
-    printf("it is leap year");
-  /*}
-  else if(year%100 && year%400){
-    printf("it is also leap year");
-  }*/
-  }else{
-    printf("it is not leap year");
-  }
+#include <stdio.h>
 
+int main() {
+    int year;
+
+    printf("Enter the year: ");
+    scanf("%d", &year);
+
+    if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        printf("It is a leap year");
+    } else {
+        printf("It is not a leap year");
+    }
+
+    return 0;
 }
